1923a4eda6ba562debb3504a107f9db73256f099
victorcas30 <vcastillo.mancia@gmail.com>
2023-08-17 12:13:09 -0600

    LeeApis

--- src/leerApis.js
+++ src/leerApis.js
@@ -2,7 +2,7 @@ export const getEmpleados = async() => {
     const headers = {"Authorization":"Bearer "};
     const options = {method:'GET',headers:headers};
     try{
-        const respuesta = await fetch("http://localhost:8000/api/empleados",options);
+        const respuesta = await fetch("https://crud-dg5b-dev.fl0.io/api/empleados",options);
         const result = await respuesta.json();
         return result;
     } catch (error) {
@@ -14,7 +14,7 @@ export const getEmpleadosConcat = async(cadenabusqueda) => {
     const headers = {"Authorization":"Bearer "};
     const options = {method:'GET',headers:headers};
     try{
-        const respuesta = await fetch(`http://localhost:8000/empleados/${cadenabusqueda}`,options);
+        const respuesta = await fetch(`https://crud-dg5b-dev.fl0.io/empleados/${cadenabusqueda}`,options);
         const result = await respuesta.json();
         return result;
     } catch (error) {
@@ -30,7 +30,7 @@ export const guardarEmpleado = async(empleadoData) => {
         body:JSON.stringify(empleadoData)
     }
     try{
-        const respuesta = await fetch("http://localhost:8000/api/empleados",options);
+        const respuesta = await fetch("https://crud-dg5b-dev.fl0.io/api/empleados",options);
         const result = await respuesta.json();
         return result;
     } catch (error) {
@@ -46,7 +46,7 @@ export const editarEmpleadoFetch = async(empleadoData) => {
         body:JSON.stringify(empleadoData)
     }
     try{
-        const respuesta = await fetch("http://localhost:8000/api/empleados",options);
+        const respuesta = await fetch("https://crud-dg5b-dev.fl0.io/api/empleados",options);
         const result = await respuesta.json();
         return result;
     } catch (error) {
@@ -62,7 +62,7 @@ export const eliminarEmpleadoFetch = async(empleadoData) => {
         body:JSON.stringify(empleadoData)
     }
     try{
-        const respuesta = await fetch("http://localhost:8000/api/empleados/delete",options);
+        const respuesta = await fetch("https://crud-dg5b-dev.fl0.io/api/empleados/delete",options);
         const result = await respuesta.json();
         return result;
     } catch (error) {
